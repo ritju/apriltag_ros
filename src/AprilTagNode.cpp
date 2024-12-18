e502c7ae8e131de47b73d142a22a54bf645352f8
zxx-0717 <1024988917@qq.com>
2024-12-18 19:49:46 +0800

    合并image_transport::CameraSubscriber到同一个launch文件中。

--- src/AprilTagNode.cpp
+++ src/AprilTagNode.cpp
@@ -90,7 +90,8 @@ private:
 
     std::function<void(apriltag_family_t*)> tf_destructor;
 
-    const image_transport::CameraSubscriber sub_cam;
+    const image_transport::CameraSubscriber sub_cam1;
+    const image_transport::CameraSubscriber sub_cam2;
     const rclcpp::Publisher<apriltag_msgs::msg::AprilTagDetectionArray>::SharedPtr pub_detections;
     tf2_ros::TransformBroadcaster tf_broadcaster;
 
@@ -146,11 +147,19 @@ AprilTagNode::AprilTagNode(const rclcpp::NodeOptions& options)
     cb_parameter(add_on_set_parameters_callback(std::bind(&AprilTagNode::onParameter, this, std::placeholders::_1))),
     td(apriltag_detector_create()),
     // topics
-    sub_cam(image_transport::create_camera_subscription(
+    sub_cam1(image_transport::create_camera_subscription(
         this,
-        this->get_node_topics_interface()->resolve_topic_name("image_rect"),
+        this->get_node_topics_interface()->resolve_topic_name("image_rect1"),
         std::bind(&AprilTagNode::onCamera, this, std::placeholders::_1, std::placeholders::_2),
-        declare_parameter("image_transport", "raw", descr({}, true)),
+        // declare_parameter("image_transport", "raw", descr({}, true)),
+        "raw",
+        rmw_qos_profile_sensor_data)),
+    sub_cam2(image_transport::create_camera_subscription(
+        this,
+        this->get_node_topics_interface()->resolve_topic_name("image_rect2"),
+        std::bind(&AprilTagNode::onCamera, this, std::placeholders::_1, std::placeholders::_2),
+        // declare_parameter("image_transport", "raw", descr({}, true)),
+        "raw",
         rmw_qos_profile_sensor_data)),
     pub_detections(create_publisher<apriltag_msgs::msg::AprilTagDetectionArray>("detections", rclcpp::QoS(1))),
     tf_broadcaster(this)
